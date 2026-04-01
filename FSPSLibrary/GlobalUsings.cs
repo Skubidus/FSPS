f4e99029d10900c74c504bd605e93fef1d5b61a5
Dennis Döring <big_atze@hotmail.com>
2026-04-01 18:12:56 +0200

    • Project files FSPSWinUI.csproj and FSPSLibrary.csproj switched to .NET 9 (net9.0-...), LangVersion set to 14.0, and Nullable enabled. • Unique ProjectGuid values added to both FSPSLibrary.csproj files. • GlobalUsings.cs added to both projects with common namespaces (including System.Diagnostics, System.Threading, System.Threading.Tasks, System.Net.Http, System.ComponentModel). • FSPSWinUI source files App.xaml.cs and MainWindow.xaml.cs converted to file-scoped namespaces. • Root .editorconfig plus project .editorconfig files (.editorconfig, .editorconfig) added to enforce file-scoped namespaces and basic code style. • Project reference FSPSWinUI -> ../FSPSLibrary/FSPSLibrary.csproj added. • Minor cleanup and consistency changes to usings/namespaces to prepare the solution for future development.

---
+++ FSPSLibrary/GlobalUsings.cs
@@ -0,0 +1,17 @@
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+
+global using Microsoft.UI.Xaml;
+global using Microsoft.UI.Xaml.Controls;
+
+global using Windows.Foundation;
+global using System.Collections.ObjectModel;
+global using System.ComponentModel;
+global using System.Diagnostics;
+global using System.IO;
+global using System.Net.Http;
+global using System.Runtime.CompilerServices;
+global using System.Threading;
---
+++ FSPSWinUI/GlobalUsings.cs
@@ -0,0 +1,20 @@
+global using System;
+global using System.Collections.Generic;
+global using System.Collections.ObjectModel;
+global using System.ComponentModel;
+global using System.Diagnostics;
+global using System.IO;
+global using System.Linq;
+global using System.Net.Http;
+global using System.Runtime.CompilerServices;
+global using System.Text;
+global using System.Threading;
+global using System.Threading.Tasks;
+
+global using Microsoft.UI.Xaml;
+global using Microsoft.UI.Xaml.Controls;
+global using Microsoft.UI.Xaml.Media;
+global using Microsoft.UI.Xaml.Navigation;
+
+global using Windows.Foundation;
+global using Windows.ApplicationModel;
